ecb8fba8f45949916dcc137f30aee7365a7b02c8
Erika Alban <erikalban12@gmail.com>
2025-04-29 16:23:09 -0400

    add a comment

--- sort.js
+++ sort.js
@@ -3,24 +3,26 @@
  * @param {number[]} heights
  * @return {string[]}
  */
-var sortPeople = function(names, heights) {
-    // group arrays
-    let people = [];
+var sortPeople = function (names, heights) {
+  // group arrays
+  let people = [];
 
-    for ( let i=0; i<names.length; i++) {
-        people.push({name: names[i], height: heights[i]})
-    }
+  for (let i = 0; i < names.length; i++) {
+    people.push({ name: names[i], height: heights[i] });
+  }
 
-    // now that the arrays are grouped I can sort in descending order
-    people.sort(function(a, b) {
-        return b.height - a.height;
-    });
-    // can also use people.sort((a, b) => b.height - a.height);
+  // now that the arrays are grouped I can sort in descending order
+  // I didn't understand the shorthand function that you provided to me for this, but now I do.
+  // I'm using the sort method but in long form, because that is what I'm used to..
+  people.sort(function (a, b) {
+    return b.height - a.height;
+  });
+  // can also use people.sort((a, b) => b.height - a.height);
 
-    let sortedNames = [];
-    for (let i = 0; i < people.length; i++) {
-        sortedNames.push(people[i].name);
-    }
+  let sortedNames = [];
+  for (let i = 0; i < people.length; i++) {
+    sortedNames.push(people[i].name);
+  }
 
-    return sortedNames;
+  return sortedNames;
 };
